5d9ead52c4814c53bd6d27c2c5e5b4f45448aced
Olga Dokukova <dokukova@gmail.com>
2021-05-28 02:45:08 +0200

    feat: resolve subtask1

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,16 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var list = sadArray.toList()
+        var sad = true
+        while (sad) {
+            val size = list.size
+            list = list.filterIndexed { index, it ->
+                index == 0 || index == list.size - 1 || it < list[index - 1] + list[index + 1]
+            }
+            sad = size != list.size
+        }
+        return list.toIntArray()
     }
 }
