dd5732e7e607be7cced1f66aa02547c10260229f
Olga Dokukova <dokukova@gmail.com>
2021-05-28 03:27:58 +0200

    feat: resolve subtask2

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -2,9 +2,13 @@ package subtask2
 
 class BillCounter {
 
-    // TODO: Complete the following function
-    // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        val fairlyBill = bill.toMutableList().apply {removeAt(k)}.sum()/2
+        return if (fairlyBill == b)
+        {
+            "Bon Appetit"
+        } else {
+            "${b -fairlyBill}"
+        }
     }
 }
